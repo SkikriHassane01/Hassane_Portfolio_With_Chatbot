94ac2f28211d31d2f30ddde7c40c128b7fc2d13e
HASSANE SKIKRI <hassaneskikri@gmail.com>
2025-03-04 14:26:54 +0000

    Add About section with animated profile and social links

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,12 +2,15 @@ import { useState } from 'react'
 import { ThemeProvider } from './context/ThemeContext'
 import Navbar from './components/layout/Navbar'
 import Footer from './components/layout/Footer'
-
+import About from './components/sections/About'
 function App() {
   return (
     <ThemeProvider>
       <div className="min-h-screen bg-white dark:bg-gray-900">
         <Navbar />
+        <main>
+          <About />
+        </main>
         <Footer />
       </div>
     </ThemeProvider>
