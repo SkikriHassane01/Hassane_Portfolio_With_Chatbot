10c6daeb7a91747b67e0c37a3548543b5eee4f9d
HASSANE SKIKRI <hassaneskikri@gmail.com>
2025-03-04 14:25:40 +0000

    Integrate ThemeProvider and add basic app layout with Navbar and Footer

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
+import { ThemeProvider } from './context/ThemeContext'
+import Navbar from './components/layout/Navbar'
+import Footer from './components/layout/Footer'
 
 function App() {
-
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider>
+      <div className="min-h-screen bg-white dark:bg-gray-900">
+        <Navbar />
+        <Footer />
+      </div>
+    </ThemeProvider>
   )
 }
 
